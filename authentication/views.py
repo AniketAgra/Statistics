0ccbe757d91ee45565ce1b2decb49189968d456f
Aniket Agrawal <aniketagra7987@gmail.com>
2024-05-04 18:54:59 +0530

    first commit

---
+++ authentication/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.urls import reverse
+from .graph_helper import get_user
+from .auth_helper import get_sign_in_url, get_token_from_code,get_token
+from django.contrib.auth.models import User
+# from .models import User
+from django.contrib.auth import authenticate, login, logout
+import re
+
+User._meta.get_field('email')._unique = True
+User._meta.get_field('username')._unique = False
+
+def initialize_context(request):
+  context = {}
+
+  # Check for any errors in the session
+  error = request.session.pop('flash_error', None)
+
+  if error != None:
+    context['errors'] = []
+    context['errors'].append(error)
+
+  # Check for user in the session
+  context['user'] = request.session.get('user', {'is_authenticated': False})
+  return context
+
+
+def sign_in(request):
+  # Get the sign-in URL
+  sign_in_url, state = get_sign_in_url()
+  # Save the expected state so we can validate in the callback
+  request.session['auth_state'] = state
+  # print(sign_in_url,state)
+  # Redirect to the Azure sign-in page
+  return HttpResponseRedirect(sign_in_url)
+def callback(request):
+  # Get the state saved in session
+  expected_state = request.session.pop('auth_state', '')
+  # Make the token request
+  # print("#######################################3\n",request.get_full_path())
+  token = get_token_from_code('https://swc.iitg.ac.in/'+request.get_full_path(), expected_state)
+  # Get the user's profile
+  user = get_user(token)
+
+  # Get user info
+  username = user['displayName'].replace(" ","_")
+  username = re.sub('[^0-9a-zA-Z_]+','',username)
+  password = user['surname']
+  email = user['mail']
+  # print(password,email)
+
+  try:
+      user = User.objects.get(email=email)
+  except User.DoesNotExist:
+      user = User.objects.create_user(username,email,password)
+      user.save()
+  user = User.objects.get(email=email)
+  # print(user)
+  if user is not None:
+      login(request,user)
+      messages.success(request,"Success: You were successfully logged in.")
+      return redirect('home:home')
+  else:
+      print("Caught an error while logging in",username,email)
+  return redirect('home:home')
+
+def sign_out(request):
+  # Clear out the user and token
+  logout(request)
+  messages.success(request, "Successfully Logged Out")
+
+  return redirect('home:home')
---
+++ static/home/wordcloud.js
@@ -0,0 +1,48 @@
+function plot_cloud(data) {
+
+    var chart;
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth,
+        height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    am4core.ready(function () {
+
+        // Themes begin
+        am4core.useTheme(am4themes_animated);
+        // Themes end
+
+
+
+        chart = am4core.create("wordcloud", am4plugins_wordCloud.WordCloud);
+
+        var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
+
+
+        series.accuracy = 4;
+        series.step = Math.round((2*width+1100)/91.0);
+        series.rotationThreshold = 0;
+        series.labels.template.tooltipText = "{word}:\n[bold]{value} placed[/]";
+        series.fontFamily = "Inter Regular";
+
+        series.data = data;
+        series.dataFields.word = "tag";
+        series.dataFields.value = "weight";
+        series.dataFields.urlval = "urlval"
+        series.labels.template.url = "/placement-portal/table/?company={urlval}";
+        series.labels.template.urlTarget = "_blank";
+
+        // series.minfontsize = ;
+        // series.maxfontsize = 70;
+        series.heatRules.push({
+            "target": series.labels.template,
+            "property": "fill",
+            "min": am4core.color("#00008B"),
+            "max": am4core.color("#d2222d"),
+            "dataField": "value"
+        });
+        chart.logo.disabled = true
+        chart.logo.height = -15000;
+    }); // end am4core.ready()
+
+
+}
+
+plot_cloud(data5);
